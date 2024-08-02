3ff92b6e40f0ee359be1b92c342d426c9b01d15d
msashraf3 <mshamimas@proton.me>
2024-08-02 23:21:07 +0600

    creating some calculation website

---
+++ geometry-genius/scripts/rectangle-calculate.js
@@ -0,0 +1,19 @@
+function rectangleCalculation(){
+    //get the rectangle width value
+    const rectangleWidth = document.getElementById('rectangle-width').value;
+    const convertingRectangleValue = parseFloat(rectangleWidth);
+    console.log(convertingRectangleValue);
+
+    //get the rectangle i value
+    const rectangleI = document.getElementById('rectangle-i').value;
+    const convertingRectangleIValue = parseFloat(rectangleI);
+    console.log(convertingRectangleIValue);
+
+    //calculating the area of the rectangle
+    const areaOfRectangle = convertingRectangleValue * convertingRectangleIValue;
+    console.log(areaOfRectangle);
+
+    //inserting the value
+    const rectangleAnswer = document.getElementById('rectangle-area');
+    rectangleAnswer.innerText = areaOfRectangle;
+}
---
+++ geometry-genius/scripts/triangle-calculate.js
@@ -0,0 +1,30 @@
+/**
+ * objective: get base, height of a triangle. Calculate the area by using the provided formula. And then display the area
+ * step-1: get base value
+ * step-2: added an id in the base input field
+ * step-3: use getElementId to access the input field
+ * step-4: get value from the input field. [value is string now]
+ * step-5: convert the value to a number using parseFloat
+ */
+function triangleCalculation(){
+    //get triangle base value
+    const base = document.getElementById('triangle-base');
+    const triangleBase = base.value;
+    const triangleValueChange = parseFloat(triangleBase);
+    console.log(triangleValueChange);
+
+    //get triangle height value
+    const height = document.getElementById('triangle-height');
+    const triangleHeight = height.value;
+    const triangHeightleValueChange = parseFloat(triangleHeight);
+    console.log(triangHeightleValueChange);
+
+    //calculate triangle area
+    const triangleArea = 0.5 * triangleValueChange * triangHeightleValueChange;
+    console.log('area of the triangle is:', triangleArea);
+
+    //inserting the value to the p tag
+    const answer = document.getElementById('area-answer');
+    answer.innerText = triangleArea;
+}
+
